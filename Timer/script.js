cc46b1f447e703e5e88f7b84483526aea0cb002d
Aashita Radhika <164779243+aashita-radhika@users.noreply.github.com>
2024-06-28 20:15:28 +0530

    Countdown

---
+++ Timer/script.js
@@ -0,0 +1,36 @@
+const endDate = "25 August 2024 12:00 AM"
+
+document.getElementById("end-Date").innerText = endDate // Dynamic 
+
+const inputs = document.querySelectorAll("input")
+
+function clock() {
+    const end = new Date (endDate)
+    const now = new Date ()
+
+    // const diff = end - now; // the difference is in millisec
+    const diff = (end - now) / 1000
+
+    //days
+    inputs[0].value = Math.floor(diff / 3600 / 24)
+    
+    //hours
+    inputs[1].value = Math.floor(diff / 3600) % 24
+
+    //minutes
+    inputs[2].value = Math.floor(diff / 60) % 60
+
+    //seconds
+    inputs[3].value = Math.floor(diff) % 60
+}
+clock()
+
+/*
+1 day = 24 hrs
+1 hr = 60 min
+1 min = 60 sec
+*/
+
+setInterval(() => {
+    clock()
+}, 1000);
